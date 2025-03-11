a6d066b4a4ea40bc29745b2e917a2ce24b1a03e6
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-03-10 23:30:16 -0700

    Update api.dart

--- lib/api.dart
+++ lib/api.dart
@@ -1,19 +1,11 @@
 import "dart:convert";
 import "dart:io";
-import "package:flutter/foundation.dart";
 import "package:http/http.dart";
 
 import "package:warm_app/util.dart";
 import "package:warm_app/const.dart";
 
 const apiString = "https://api.airgradient.com/public/api/v1/locations/measures/current?token={token}";
-
-class Compute {
-  final String responseBody;
-  final Map<int, String> locationIds;
-
-  const Compute(this.responseBody, this.locationIds);
-}
 
 Future<Map<String, Map<String, num>>> getCurrentMonitorDataFromAllLocations(String token, Map<int, String> locationIds) async {
   String uri = apiString.replaceAll("{token}", token);
@@ -22,7 +14,7 @@ Future<Map<String, Map<String, num>>> getCurrentMonitorDataFromAllLocations(Stri
     final response = await get(Uri.parse(uri));
 
     if (response.statusCode == HttpStatus.ok) {
-      return await compute(getProcessedCurrentMonitorData, Compute(response.body, locationIds));
+      return getProcessedCurrentMonitorData(response.body, locationIds);
     } else {
       throw HttpException("Failed to load data: ${response.statusCode}");
     }
@@ -32,30 +24,33 @@ Future<Map<String, Map<String, num>>> getCurrentMonitorDataFromAllLocations(Stri
   }
 }
 
-Map<String, Map<String, num>> getProcessedCurrentMonitorData(Compute data) {
-  List<Map<String, dynamic>> monitors = List<Map<String, dynamic>>.from(json.decode(data.responseBody));
+Map<String, Map<String, num>> getProcessedCurrentMonitorData(String responseBody, Map<int, String> locationIds) {
+  List<dynamic> monitors = json.decode(responseBody);
+  final monitorData = <String, Map<String, num>>{};
 
-  Map<String, Map<String, num>> monitorData = {};
+  for (final monitor in monitors) {
+    try {
+      final int locationId = monitor['locationId'];
+      final String locationName = monitor['locationName'];
 
-  for (Map<String, dynamic> monitor in monitors) {
-    int locationId = monitor["locationId"];
-    String locationName = monitor["locationName"];
-    
-    if (data.locationIds.containsKey(locationId)) {
-      monitorData[locationName] = {
-        "PM2.5": monitor["pm02"] ?? 0,
-        "PM10": monitor["pm10"] ?? 0,
-        "TVOC": monitor["tvocIndex"] ?? 0,
-        "CO2": monitor["rco2"] ?? 0,
-        "RH": monitor["rhum"] ?? 0,
-        "T": monitor["atmp"] ?? 0,
-        "pm003Count": monitor["pm003Count"] ?? 0,
-        "plantower": convertPTSerialToNum(data.locationIds[locationId] ?? defaultPTSerial),
-      };
+      if (locationIds.containsKey(locationId)) {
+        monitorData[locationName] = {
+          'PM2.5': monitor['pm02'] ?? 0,
+          'PM10': monitor['pm10'] ?? 0,
+          'TVOC': monitor['tvocIndex'] ?? 0,
+          'CO2': monitor['rco2'] ?? 0,
+          'RH': monitor['rhum'] ?? 0,
+          'T': monitor['atmp'] ?? 0,
+          'pm003Count': monitor['pm003Count'] ?? 0,
+          'plantower': convertPTSerialToNum(locationIds[locationId] ?? defaultPTSerial),
+        };
 
-      applyCorrectionsToRawData(monitorData[locationName]!);
+        applyCorrectionsToRawData(monitorData[locationName]!);
+      }
+    } catch (e) {
+      print('Error processing monitor data: $e');
     }
   }
-
+  
   return monitorData;
 }
